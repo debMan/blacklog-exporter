5d8f79bc9e14c86a91c48981269bf627e8ad341b
MohamadAli Rezaie <mohamadali.rezaie@snapp.cab>
2024-09-22 14:05:06 +0330

    fix: Update configuration structs

--- internal/kafkaclient/config.go
+++ internal/kafkaclient/config.go
@@ -10,5 +10,5 @@ type Config struct {
 	SaslPassword     string   `json:"sasl_password"         koanf:"sasl_password"`
 	SaslUsername     string   `json:"sasl_username"         koanf:"sasl_username"`
 	SecurityProtocol string   `json:"security_protocol"     koanf:"security_protocol"`
-	Topics           []string `json:"topic"                 koanf:"topic"`
+	Topics           []string `json:"topics"                koanf:"topics"`
 }
